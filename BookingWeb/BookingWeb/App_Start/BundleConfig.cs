f289863fa8f2d502d6b1b26673563d24f02d2fb8
Sergei <hmaster20@gmail.com>
2017-06-28 20:03:07 +0300

    test2

---
+++ BookingWeb/BookingWeb/App_Start/BundleConfig.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Optimization;
+
+namespace BookingWeb.App_Start
+{
+    public class BundleConfig
+    {
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            // ...
+
+            // New code:
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                      "~/Scripts/knockout-{version}.js",
+                      "~/Scripts/app.js"));
+        }
+
+    }
+}
